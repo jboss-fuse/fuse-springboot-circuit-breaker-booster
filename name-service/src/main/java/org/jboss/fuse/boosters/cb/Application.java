eb543deb25af711a1b05e2e489f9c36a0f1b2ec4
Aurélien Pupier <apupier@redhat.com>
2018-02-01 17:08:00 +0100

    Remove unused import package
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- name-service/src/main/java/org/jboss/fuse/boosters/cb/Application.java
+++ name-service/src/main/java/org/jboss/fuse/boosters/cb/Application.java
@@ -2,9 +2,6 @@ package org.jboss.fuse.boosters.cb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.undertow.UndertowBuilderCustomizer;
-import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
